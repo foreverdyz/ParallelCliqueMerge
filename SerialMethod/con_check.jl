9b2490b3215e5178e437d4937676f655aca46a0b
foreverdyz <44935367+foreverdyz@users.noreply.github.com>
2023-10-08 11:10:36 -0400

    Update con_check.jl

--- SerialMethod/con_check.jl
+++ SerialMethod/con_check.jl
@@ -1,9 +1,13 @@
 #con_check.jl
 
-function _is_feasible(a::SparseVector, b::Real)
+function _is_feasible_or_set_pack(a::SparseVector, b::Real)
     s = 0
+    (b == 1) ? (is_set_pack = true) : (is_set_pack = false)
     for i in findnz(a)[2]
         s += min(0, i)
+        if is_set_pack
+            (i != 1) && (is_set_pack = false)
+        end
     end
-    (s < b + 0.00001) ? (return true) : (return false)
+    (s < b + 0.00001) ? (return true, is_set_pack) : (return false, false)
 end
