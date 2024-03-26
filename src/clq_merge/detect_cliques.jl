ce84da593440a78ff57d137bbeeb1a773dee66e2
foreverdyz <44935367+foreverdyz@users.noreply.github.com>
2024-03-26 17:10:53 -0400

    Update detect_cliques.jl

--- src/clq_merge/detect_cliques.jl
+++ src/clq_merge/detect_cliques.jl
@@ -22,9 +22,8 @@ function detect_cliques_parallel(knapsack_set::Vector{Tuple{Any, Any, Any}}, num
     numthreads = min(m, numthreads)
     numthreads = max(1, numthreads)
     width = floor(Int64, m / numthreads)
-    spl = SpinLock()
-    clique_set_g = [Int64[] for _ in 1:length(knapsack_set)]
-    main_cliques_g = [Vector{Int64}[] for _ in 1:length(knapsack_set)]
+    clique_set_g = [Vector{Int64}[] for _ in 1:length(knapsack_set)]
+    main_cliques_g = [Int64[] for _ in 1:length(knapsack_set)]
     index_set = shuffle(1:m)
     @threads for id in 1:numthreads
         let
