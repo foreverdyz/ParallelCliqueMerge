daa02e54ac72f3d1db6d63aea99999d548e57153
foreverdyz <44935367+foreverdyz@users.noreply.github.com>
2025-03-29 22:55:50 -0400

    Delete src/presolve.sh

--- src/presolve.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-julia --threads 1 presolve.jl 1
-julia --threads 2 presolve.jl 2
-julia --threads 4 presolve.jl 4
-julia --threads 8 presolve.jl 8
-julia --threads 16 presolve.jl 16
-julia --threads 12 presolve.jl 12
